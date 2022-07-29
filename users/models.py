4aec81d6392d1900907d324b33124d045d3bc600
unknown <chepurnojv@gmail.com>
2022-07-29 17:12:43 +0300

    User Registration

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ import uuid
 # Create your models here.
 
 
-
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, null=True, blank=True)
@@ -26,7 +25,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
